793c779638ebc11f370561a9996b3c0c151f23ab
Cameron Smith <smithc11@rpi.edu>
2019-05-15 11:12:15 -0700

    add all reduce calls

--- capi.cc
+++ capi.cc
@@ -1,9 +1,14 @@
+#include <mpi.h>
 #include "capi.h"
 #include <iostream>
 
 int pass_by_val(int num)
 {
 	std::cout << num << std::endl;
+        int in = num;
+        int out;
+        for(int i=0; i<10; i++)
+          MPI_Allreduce(&in,&out,1,MPI_INTEGER,MPI_SUM,MPI_COMM_WORLD);
 	return 0;
 }
 
--- main.F90
+++ main.F90
@@ -3,9 +3,10 @@ program fortran_c_api
   use iso_c_binding
 	include 'mpif.h'
 	integer(c_int) stat, num
-	integer err, mstat
+	integer err, mstat, rank
 	call mpi_init(err)
-	num = 1000
+	call mpi_comm_rank(MPI_COMM_WORLD,rank,err)
+	num = 1000 + rank
 	stat = pass_by_val(num)
 	call mpi_finalize(err)
 end program fortran_c_api
